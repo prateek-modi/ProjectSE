fcde25ee8ee44fc58ab9c4c85aa15a25045f1aa3
dvkcool <divyanshukumarg@gmail.com>
2019-03-18 13:17:22 +0530

    Added database pool connection and working server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+require('request-debug')(request);
+var fetch =  require('fetch');
+var mysql = require('mysql');
+var config = require("./config.json");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname + '/public'));
+
+var pool = mysql.createPool(config);
+
+app.get('/pooltest', (req,res)=>{
+  pool.query('SELECT * from bill;', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+    res.send(results);
+  });
+});
+app.listen(8083, function () {
+  console.log('Example app listening on port 8080!');
+});
