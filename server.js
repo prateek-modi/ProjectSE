1fb649687266be30e46e679dee9df4925216ddff
vaibnak <vaibnakgupta100@gmail.com>
2019-03-20 20:58:11 +0530

    demo added

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // app.use(express.static(__dirname + '/public'));
 // // var pool = mysql.createPool(config);
 //
